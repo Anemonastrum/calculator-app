9efd828110f7ff17d7d0c4227cf35de0f6d98fe4
Tirta <d.ermaya@amikom.ac.id>
2026-04-09 10:09:54 +0700

    menlengkapi main ges

--- calculator.cs
+++ calculator.cs
@@ -1,1 +1,15 @@
 class Calculator
+{
+	static void Main(string[] args)
+	{
+		int a = 10;
+		int b = 6;
+
+		Console.WriteLine("Penambahan", a,b, Penambahan(a,b));
+		Console.WriteLine("Pengurangan", a,b Pengurangan(a,b));
+
+		Console.WriteLine("\n Press any key");
+		Console.ReadKey();
+	}
+
+}
